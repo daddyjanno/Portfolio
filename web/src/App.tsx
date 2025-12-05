edc165efc578274d5d2ba2f820f00bf77ee32bef
daddyjanno <jn.drugmand@gmail.com>
2025-12-05 10:56:41 +0100

    App

---
+++ web/src/App.tsx
@@ -0,0 +1,33 @@
+import { useDarkMode } from './hooks/useDarkMode';
+import { Navbar } from './components/common/Navbar/Navbar';
+import { Hero } from './components/sections/Hero/Hero';
+import { About } from './components/sections/About/About';
+import { Skills } from './components/sections/Skills/Skills';
+import { Projects } from './components/sections/Projects/Projects';
+import { Timeline } from './components/sections/Timeline/Timeline';
+import { Contact } from './components/sections/Contact/Contact';
+import { Footer } from './components/common/Footer/Footer';
+import './styles/global.scss';
+
+function App() {
+  const { isDark, toggleDarkMode } = useDarkMode();
+
+  return (
+    <div className="app">
+      <Navbar isDark={isDark} onToggleDarkMode={toggleDarkMode} />
+
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Timeline />
+        <Contact />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
