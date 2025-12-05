66289ae6eac050efe8058bd1772c984079d6352b
daddyjanno <jn.drugmand@gmail.com>
2025-12-05 10:56:30 +0100

    skillLogos

---
+++ web/src/utils/skillLogos.tsx
@@ -0,0 +1,321 @@
+/* Skill logos with links to documentation */
+
+export const SkillLogos = {
+  // Frontend
+  html5: (
+    <a
+      href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/skills/html5-colored.svg"
+        width="36"
+        height="36"
+        alt="HTML5"
+      />
+    </a>
+  ),
+  css3: (
+    <a href="https://www.w3.org/TR/CSS/#css" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/skills/css3-colored.svg"
+        width="36"
+        height="36"
+        alt="CSS3"
+      />
+    </a>
+  ),
+  javascript: (
+    <a
+      href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/skills/javascript-colored.svg"
+        width="36"
+        height="36"
+        alt="JavaScript"
+      />
+    </a>
+  ),
+  typescript: (
+    <a href="https://typescript.com/" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/skills/typescript-colored.svg"
+        width="36"
+        height="36"
+        alt="Typescript"
+      />
+    </a>
+  ),
+  react: (
+    <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/skills/react-colored.svg"
+        width="36"
+        height="36"
+        alt="React"
+      />
+    </a>
+  ),
+  redux: (
+    <a href="https://redux.js.org/" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/skills/redux-colored.svg"
+        width="36"
+        height="36"
+        alt="Redux"
+      />
+    </a>
+  ),
+  sass: (
+    <a href="https://sass-lang.com/" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/skills/sass-colored.svg"
+        width="36"
+        height="36"
+        alt="Sass"
+      />
+    </a>
+  ),
+  styledComponents: (
+    <a href="https://styled-components.com/" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/refs/heads/main/skills/styled-components.svg"
+        width="36"
+        height="36"
+        alt="Styled-components"
+      />
+    </a>
+  ),
+  tailwindcss: (
+    <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/skills/tailwindcss-colored.svg"
+        width="36"
+        height="36"
+        alt="Tailwind CSS"
+      />
+    </a>
+  ),
+  bootstrap: (
+    <a href="https://getbootstrap.com/" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/skills/bootstrap-colored.svg"
+        width="36"
+        height="36"
+        alt="Bootstrap"
+      />
+    </a>
+  ),
+  nextjs: (
+    <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/93bdd5cda33fd91792d741f0e7e87d7d06de30bb/skills/nextjs-colored-dark.svg"
+        width="36"
+        height="36"
+        alt="NextJS"
+      />
+    </a>
+  ),
+  pixijs: (
+    <a href="https://pixijs.com/" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/refs/heads/main/skills/pixijs-colored-dark.svg"
+        width="36"
+        height="36"
+        alt="PixiJS"
+      />
+    </a>
+  ),
+  animejs: (
+    <a href="https://animejs.com/" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/refs/heads/main/skills/animejs.jpg"
+        width="36"
+        height="36"
+        alt="AnimeJS"
+      />
+    </a>
+  ),
+
+  // Backend
+  nodejs: (
+    <a href="https://nodejs.org/en/" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/skills/nodejs-colored.svg"
+        width="36"
+        height="36"
+        alt="NodeJS"
+      />
+    </a>
+  ),
+  ruby: (
+    <a href="https://ruby-lang.org/" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/93bdd5cda33fd91792d741f0e7e87d7d06de30bb/skills/ruby-colored.svg"
+        width="36"
+        height="36"
+        alt="Ruby"
+      />
+    </a>
+  ),
+  rails: (
+    <a href="https://rubyonrails.org/" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/93bdd5cda33fd91792d741f0e7e87d7d06de30bb/skills/rails-colored-svg.svg"
+        width="36"
+        height="36"
+        alt="Rails"
+      />
+    </a>
+  ),
+  mongodb: (
+    <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/skills/mongodb-colored.svg"
+        width="36"
+        height="36"
+        alt="MongoDB"
+      />
+    </a>
+  ),
+  mysql: (
+    <a href="https://www.mysql.com/" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/skills/mysql-colored.svg"
+        width="36"
+        height="36"
+        alt="MySQL"
+      />
+    </a>
+  ),
+  postgresql: (
+    <a href="https://www.postgresql.org/" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/skills/postgresql-colored.svg"
+        width="36"
+        height="36"
+        alt="PostgreSQL"
+      />
+    </a>
+  ),
+
+  // Tools & Design
+  heroku: (
+    <a href="https://www.heroku.com/" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/skills/heroku-colored.svg"
+        width="36"
+        height="36"
+        alt="Heroku"
+      />
+    </a>
+  ),
+  figma: (
+    <a href="https://www.figma.com/" target="_blank" rel="noreferrer">
+      <img
+        src="/figma-icon.svg"
+        width="36"
+        height="36"
+        alt="Figma"
+      />
+    </a>
+  ),
+  macos: (
+    <a href="https://apple.com" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/skills/macos.svg"
+        width="36"
+        height="36"
+        alt="MacOS"
+      />
+    </a>
+  ),
+
+  // Creative
+  photoshop: (
+    <a href="https://www.adobe.com/uk/products/photoshop.html" target="_blank" rel="noreferrer">
+      <img
+        src="/adobe-photoshop-icon.svg"
+        width="36"
+        height="36"
+        alt="Photoshop"
+      />
+    </a>
+  ),
+  aftereffects: (
+    <a href="https://www.adobe.com/uk/products/aftereffects.html" target="_blank" rel="noreferrer">
+      <img
+        src="/adobe-after-effects-icon.svg"
+        width="36"
+        height="36"
+        alt="After Effects"
+      />
+    </a>
+  ),
+  premierepro: (
+    <a href="https://www.adobe.com/uk/products/premiere.html" target="_blank" rel="noreferrer">
+      <img
+        src="/adobe-premiere-pro-icon.svg"
+        width="36"
+        height="36"
+        alt="Premiere Pro"
+      />
+    </a>
+  ),
+  finalcut: (
+    <a href="https://www.apple.com/final-cut-pro/" target="_blank" rel="noreferrer">
+      <img
+        src="/finalcut.png"
+        width="36"
+        height="36"
+        alt="Final Cut Pro"
+      />
+    </a>
+  ),
+  avid: (
+    <a href="https://www.avid.com/" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/skills/avid-colored.svg"
+        width="36"
+        height="36"
+        alt="Avid"
+      />
+    </a>
+  ),
+
+  // Social Media
+  github: (
+    <a href="https://github.com/daddyjanno" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/socials/github-colored.svg"
+        width="36"
+        height="36"
+        alt="GitHub"
+      />
+    </a>
+  ),
+  linkedin: (
+    <a href="https://linkedin.com/in/jean-noel-drugmand" target="_blank" rel="noreferrer">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/socials/linkedin-colored.svg"
+        width="36"
+        height="36"
+        alt="LinkedIn"
+      />
+    </a>
+  ),
+  email: (
+    <a href="mailto:jn.drugmand@gmail.com">
+      <img
+        src="https://raw.githubusercontent.com/daddyjanno/Icons/main/socials/email-colored.svg"
+        width="36"
+        height="36"
+        alt="Email"
+      />
+    </a>
+  ),
+};
