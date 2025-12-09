37f28b35321ee3bc0ed3cd640bd068c4499dc03d
daddyjanno <jn.drugmand@gmail.com>
2025-12-09 20:11:35 +0100

    companyLogos

---
+++ web/src/utils/companyLogos.tsx
@@ -0,0 +1,57 @@
+/* Company logos with links */
+
+export const CompanyLogos = {
+  openclassrooms: (
+    <a href="https://openclassrooms.com/" target="_blank" rel="noreferrer">
+      <img
+        src="/openclassrooms-icon.svg"
+        width="24"
+        height="24"
+        alt="OpenClassrooms"
+        loading="lazy"
+      />
+    </a>
+  ),
+  playplay: (
+    <a href="https://playplay.com/" target="_blank" rel="noreferrer">
+      <img src="/PlayPlay.svg" width="24" height="24" alt="PlayPlay" loading="lazy" />
+    </a>
+  ),
+  lewagon: (
+    <a href="https://www.lewagon.com/" target="_blank" rel="noreferrer">
+      <img
+        src="https://github.com/daddyjanno/Icons/raw/main/schools/lewagon.svg"
+        width="24"
+        height="24"
+        alt="PlayPlay"
+        loading="lazy"
+      />
+    </a>
+  ),
+  gretadeshautsdeseine: (
+    <a href="https://www.greta-cfa92.ac-versailles.fr/" target="_blank" rel="noreferrer">
+      <img src="/greta.webp" width="24" height="24" alt="GRETA" loading="lazy" />
+    </a>
+  ),
+  eurosport: (
+    <a href="https://www.eurosport.com/" target="_blank" rel="noreferrer">
+      <img src="/eurosport.webp" width="24" height="24" alt="Eurosport" loading="lazy" />
+    </a>
+  ),
+  freelance: (
+    <a href="#" target="_blank" rel="noreferrer">
+      <svg
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        {/* Briefcase icon for freelance */}
+        <rect x="2" y="7" width="20" height="13" rx="1" />
+        <path d="M6 7V5a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v2" />
+        <line x1="12" y1="12" x2="12" y2="17" />
+      </svg>
+    </a>
+  ),
+};
